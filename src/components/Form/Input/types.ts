725f05b5a4eedecaf16b04b0402a66f5347ea20c
Your Name <you@example.com>
2025-09-08 18:32:07 +0330

    Fix issue in input type

--- src/components/Form/Input/types.ts
+++ src/components/Form/Input/types.ts
@@ -2,13 +2,13 @@
 
 import { Size } from '@/types/size.type'
 import { InputHTMLAttributes, ReactNode } from 'react'
-import { ControllerRenderProps, FieldValues, FormState, ControllerFieldState } from 'react-hook-form'
+import { ControllerRenderProps, FieldValues, ControllerFieldState, Path, UseFormStateReturn } from 'react-hook-form'
 
 export type InputTypes = 'text' | 'password'
 
 export type InputProps<IFormInput extends FieldValues> = InputHTMLAttributes<HTMLInputElement> & {
-	field: ControllerRenderProps<FieldValues, string>
-	formState: FormState<IFormInput>
+	field: ControllerRenderProps<IFormInput, Path<IFormInput>>
+	formState: UseFormStateReturn<IFormInput>
 	fieldState: ControllerFieldState
 	label?: string
 	required?: boolean
